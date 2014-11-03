66ec06d77651458cbde2682fc9e4a6e97b8f25b6
Greg V <greg@unrelenting.technology>
2014-11-03 20:21:10 +0300

    stuff

--- haskell/ghci.conf.hs
+++ haskell/ghci.conf.hs
@@ -7,21 +7,8 @@
 
 :set -DGHC_INTERACTIVE -- http://www.mega-nerd.com/erikd/Blog/CodeHacking/Haskell/ghci-trick.html
 :set -XNoMonomorphismRestriction
+:set -XOverloadedStrings
 :set +m
-
-import qualified IPPrint
-import qualified Language.Haskell.HsColour as HsColour
-import qualified Language.Haskell.HsColour.Colourise as HsColour
-import qualified Language.Haskell.HsColour.Output as HsColour
-
-let myColourPrefs = HsColour.defaultColourPrefs { HsColour.conid = [HsColour.Foreground HsColour.Yellow, HsColour.Bold], HsColour.conop = [HsColour.Foreground HsColour.Yellow], HsColour.string = [HsColour.Foreground HsColour.Green], HsColour.char = [HsColour.Foreground HsColour.Cyan], HsColour.number = [HsColour.Foreground HsColour.Red, HsColour.Bold], HsColour.layout = [HsColour.Foreground HsColour.White], HsColour.keyglyph = [HsColour.Foreground HsColour.White] }
-
-let myPrint = putStrLn . HsColour.hscolour (HsColour.TTYg HsColour.XTerm256Compatible) myColourPrefs False False "" False . IPPrint.pshow
-
-:set -interactive-print=myPrint
-:def! load \x -> return $ ":load " ++ x ++ "\n:set -interactive-print=myPrint"
-:def! module \x -> return $ ":module " ++ x ++ "\n:set -interactive-print=myPrint"
-:def! add \x -> return $ ":add " ++ x ++ "\n:set -interactive-print=myPrint"
 
 :set prompt "\ESC[1;34m[ \ESC[1;32m%s \ESC[1;34m]\n\955\ESC[m "
 -- :set prompt2 "|\ESC[m "
