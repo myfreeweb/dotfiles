f0a03338ce43c3317154a1ca79d48f8aac7dd549
Greg V <greg@unrelenting.technology>
2014-11-14 15:13:24 +0300

    lein, gpg-demasterify

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -5,14 +5,16 @@
 ; thanks:
 ; http://z.caudate.me/give-your-clojure-workflow-more-flow/
 
-{:user {:dependencies [[leiningen "2.1.2"]
+{:user {:signing {:gpg-key "greg@unrelenting.technology"}
+        :dependencies [[leiningen #=(leiningen.core.main/leiningen-version)]
                        [org.clojure/tools.namespace "0.2.4"]
-                       [im.chit/vinyasa "0.1.7"]
-                       [io.aviso/pretty "0.1.8"]
-                       [spyscope "0.1.4"]
+                       [im.chit/vinyasa "0.2.2"]
+                       [im.chit/iroh "0.1.11"]
+                       [io.aviso/pretty "0.1.12"]
+                       [spyscope "0.1.5"]
                        [ritz/ritz-nrepl-middleware "0.7.0"]
-                       [slamhound "1.5.0"]]
-        :plugins [[lein-pprint "1.1.1"]
+                       [slamhound "1.5.5"]]
+        :plugins [[lein-pprint "1.1.2"]
                   [lein-clojars "0.9.1"]
                   [lein-localrepo "0.5.3"]
                   [lein-sub "0.3.0"]
@@ -20,25 +22,25 @@
                   [lein-create-template "0.1.1"]
                   [lein-deps-tree "0.1.2"]
                   [lein-ritz "0.7.0"]
-                  [lein-light-nrepl "0.0.9"]]
+                  [lein-light-nrepl "0.1.0"]]
          :aliases {"slamhound" ["run" "-m" "slam.hound"]}}
          :injections [(require 'spyscope.core)
-                      (require 'vinyasa.inject)
-                      (vinyasa.inject/inject 'clojure.core
-                        '[[vinyasa.inject [inject inject]]
-                          [vinyasa.pull [pull pull]]
-                          [vinyasa.lein [lein lein]]
-                          [clojure.repl apropos dir doc find-doc source [root-cause cause]]
-                          [clojure.tools.namespace.repl [refresh refresh]]
-                          [clojure.pprint [pprint >pprint]]
-                          [io.aviso.binary [write-binary >bin]]])
-                      (require 'io.aviso.repl
-                               'clojure.repl
-                               'clojure.main)
-                      (alter-var-root #'clojure.main/repl-caught
-                        (constantly @#'io.aviso.repl/pretty-pst))
-                      (alter-var-root #'clojure.repl/pst
-                        (constantly @#'io.aviso.repl/pretty-pst))]
+                      (require 'io.aviso.repl)
+                      (require '[vinyasa.inject :as inject])
+                      (inject/in
+                        [vinyasa.inject :refer [inject [in inject-in]]]
+                        [vinyasa.lein :exclude [*project*]]
+                        [vinyasa.pull :all]
+                        [cemerick.pomegranate add-classpath get-classpath resources]
+
+                        clojure.core
+                        [iroh.core .> .? .* .% .%>]
+
+                        clojure.core >
+                        [clojure.pprint pprint]
+                        [clojure.java.shell sh])
+                      ]
          :repl-options {:nrepl-middleware [lighttable.nrepl.handler/lighttable-ops
+                                           io.aviso.nrepl/pretty-middleware
                                            ritz.nrepl.middleware.javadoc/wrap-javadoc
                                            ritz.nrepl.middleware.apropos/wrap-apropos]}}
