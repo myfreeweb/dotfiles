1c890642b859dcbba334e4bc7b2378b560176906
Greg V <greg@unrelenting.technology>
2015-01-01 18:04:28 +0300

    tmux mail indicator, git log gpg, shellcheck fixes, stuff

--- tmux/tmux-helper.sh
+++ tmux/tmux-helper.sh
@@ -3,35 +3,43 @@
 #      https://github.com/myfreeweb/dotfiles
 # Feel free to steal it, but attribution is nice
 
-case $1 in
+MAIL_INBOX="$HOME/Mail/INBOX/new"
+
+case "$1" in
 	launch-shell)
-		if [ "`uname`" = "Darwin" ]; then
-			if [ -x "`which reattach-to-user-namespace`" ]; then
-				exec reattach-to-user-namespace -l $2
+		if [ "$(uname)" = "Darwin" ]; then
+			if [ -x "$(which reattach-to-user-namespace)" ]; then
+				exec reattach-to-user-namespace -l "$2"
 			else
 				echo "install reattach-to-user-namespace!"
-				exec $2
+				exec "$2"
 			fi
 		else
-			exec $2
+			exec "$2"
 		fi ;;
 
-	top) if [ -x "`which htop`" ]; then exec htop; else exec top; fi ;;
+	top) if [ -x "$(which htop)" ]; then exec htop; else exec top; fi ;;
 
-	copy-buf) if [ -x "`which reattach-to-user-namespace`" ] && [ -x "`which pbcopy`" ]; then
+	mail) if [ -d "$MAIL_INBOX" ]; then
+			MAIL_COUNT="$(ls "$MAIL_INBOX" | wc -l | sed -e 's/^[ \t]*//')"
+			[ "$MAIL_COUNT" != "0" ] && printf " ✉ %s " "$MAIL_COUNT"
+		fi ;;
+
+	copy-buf) if [ -x "$(which reattach-to-user-namespace)" ] && [ -x "$(which pbcopy)" ]; then
 		tmux save-buffer - | reattach-to-user-namespace pbcopy
-	elif [ -x "`which xclip`" ]; then
+	elif [ -x "$(which xclip)" ]; then
 		tmux save-buffer - | xclip
 	else
 		echo "Copy not supported"
 	fi ;;
 
-	copy-pipe) if [ -x "`which reattach-to-user-namespace`" ] && [ -x "`which pbcopy`" ]; then
+	copy-pipe) if [ -x "$(which reattach-to-user-namespace)" ] && [ -x "$(which pbcopy)" ]; then
 		exec reattach-to-user-namespace pbcopy
-	elif [ -x "`which xclip`" ]; then
+	elif [ -x "$(which xclip)" ]; then
 		exec xclip
 	else
 		echo "Copy not supported"
 	fi ;;
-*) echo "Unknown command";;
+
+	*) echo "Unknown command";;
 esac
---
+++ vim/ftplugin/cs.vim
@@ -0,0 +1,11 @@
+" This came from Greg V's dotfiles:
+"      https://github.com/myfreeweb/dotfiles
+" Feel free to steal it, but attribution is nice
+" 
+" Thanks: see vimrc
+
+setlocal tabstop=4
+setlocal softtabstop=4
+setlocal shiftwidth=4
+setlocal smarttab
+setlocal expandtab
