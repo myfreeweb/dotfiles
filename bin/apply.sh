1bd0783bfe74fc1a147d3ba00dd3210d928074de
Greg V <greg@unrelenting.technology>
2015-12-13 14:05:46 +0300

    tmux 2.1 mouse conf; go binaries

--- bin/apply.sh
+++ bin/apply.sh
@@ -13,5 +13,12 @@ export GOBIN=~/.local/bin
 go get github.com/motemen/ghq
 go get github.com/peco/peco/cmd/peco
 go get github.com/monochromegane/the_platinum_searcher/cmd/pt
+go get github.com/rakyll/boom
+go get github.com/schachmat/wego
+go get github.com/alexanderGugel/wsd
+go get github.com/jingweno/ccat
+go get github.com/asciinema/asciinema
+go get github.com/dinedal/textql/cmd && mv ~/.local/bin/cmd ~/.local/bin/textql
+go get github.com/michaeldv/mop && mv ~/.local/bin/cmd ~/.local/bin/mop
 
 echo "==> Installed bin"
