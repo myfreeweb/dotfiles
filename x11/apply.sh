d0ef5b7a57b17502c18879fd8285eeb8bb7059c2
Greg V <greg@unrelenting.technology>
2017-09-25 19:35:40 +0300

    compton.conf

--- x11/apply.sh
+++ x11/apply.sh
@@ -55,6 +55,8 @@ cat gtk3.css > ~/.config/gtk-3.0/gtk.css
 mkdir -p ~/.config/fontconfig
 cat fonts.xml > ~/.config/fontconfig/fonts.conf
 
+cat compton.conf > ~/.config/compton.conf
+
 ln -sf ../../../.config/mimeapps.list ~/.local/share/applications/mimeapps.list
 xdg-mime default feh.desktop image/jpeg image/png image/bmp image/x-bmp image/gif image/tiff image/x-pcx
 xdg-mime default mpv.desktop application/ogg application/x-ogg video/mpeg video/x-mpeg video/x-mpeg2 video/mp4 video/msvideo video/x-msvideo video/ogg video/quicktime video/vnd.rn-realvideo video/x-ms-afs video/x-ms-asf video/x-ms-wmv video/x-ms-wmx video/x-ms-wvxvideo video/x-avi video/x-fli video/x-flv video/x-theora video/x-matroska video/webm video/x-ogm+ogg
