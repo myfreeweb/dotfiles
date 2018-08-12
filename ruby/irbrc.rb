64424f20c25eb38a83abb8b135a5c000800a557b
Greg V <greg@unrelenting.technology>
2018-08-13 00:54:02 +0300

    cleanup irb

--- ruby/irbrc.rb
+++ ruby/irbrc.rb
@@ -102,27 +102,6 @@ def bm(repetitions=100, &block)
   nil
 end
 
-def copy(str)
-  IO.popen('pbcopy', 'w') { |f| f << str.to_s }
-end
-
-def copy_history
-  history = Readline::HISTORY.entries
-  index = history.rindex("exit") || -1
-  content = history[(index+1)..-2].join("\n")
-  puts content
-  copy content
-end
-
-def paste
-  `pbpaste`
-end
-
-def clear
-  system 'clear'
-end
-alias c clear
-
 irb_require 'wirble' do
   Wirble.init
   Wirble.colorize
