8c655fae3b8e206010101be90a5f3622c4b8ecad
Greg V <greg@unrelenting.technology>
2019-10-16 22:44:06 +0300

    git stuff

--- dev-base/apply.sh
+++ dev-base/apply.sh
@@ -3,7 +3,18 @@ echo "==> Installing dev-base"
 
 mkdir -p ~/.tmp
 
+git_name="$(git config --global user.name)"
+git_email="$(git config --global user.email)"
 cat gitconfig > ~/.gitconfig
+git config --global user.name "$git_name"
+git config --global user.email "$git_email"
+# custom less flag to skip adding no-init flag which ruins page up/down
+[ -x "$(command -v delta)" ] && \
+	git config --global core.pager "env BAT_PAGER='less -R' delta --plus-color=#104010 --theme='zenburn'"
+[ -x "$(command -v interactive-rebase-tool)" ] && \
+	git config --global sequence.editor interactive-rebase-tool
+[ -x "$(command -v /usr/local/share/git-core/contrib/credential/libsecret/git-credential-libsecret)" ] && \
+	git config --global credential.helper /usr/local/share/git-core/contrib/credential/libsecret/git-credential-libsecret
 
 cat gitignore > ~/.gitignore
 
