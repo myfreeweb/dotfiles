1b1de0b516eb014893a8c1c0879aa67cbb52bdb4
Greg V <greg@unrelenting.technology>
2021-05-31 16:10:12 +0300

    desktop stuff

--- desktop/apply.sh
+++ desktop/apply.sh
@@ -26,8 +26,8 @@ xdg-mime default eog.desktop image/jpeg image/png image/bmp image/x-bmp image/gi
 xdg-mime default io.github.GnomeMpv.desktop application/ogg application/x-ogg video/mpeg video/x-mpeg video/x-mpeg2 video/mp4 video/msvideo video/x-msvideo video/ogg video/quicktime video/vnd.rn-realvideo video/x-ms-afs video/x-ms-asf video/x-ms-wmv video/x-ms-wmx video/x-ms-wvxvideo video/x-avi video/x-fli video/x-flv video/x-theora video/x-matroska video/webm video/x-ogm+ogg
 xdg-mime default org.gnome.Nautilus.desktop inode/directory
 xdg-mime default org.gnome.Evince.desktop application/pdf image/vnd.djvu image/x-djvu application/x-dvi application/vnd.comicbook+zip application/vnd.comicbook-rar application/oxps application/vnd.ms-xpsdocument application/postscript application/eps application/x-eps image/eps image/x-eps
-xdg-mime default libreoffice-writer.desktop application/vnd.oasis.opendocument.text application/vnd.oasis.opendocument.text-template application/vnd.oasis.opendocument.text-web application/vnd.oasis.opendocument.text-master application/vnd.oasis.opendocument.text-master-template
-xdg-mime default libreoffice-calc.desktop application/vnd.oasis.opendocument.spreadsheet application/vnd.oasis.opendocument.spreadsheet-template
+xdg-mime default libreoffice-writer.desktop application/vnd.oasis.opendocument.text application/vnd.oasis.opendocument.text-template application/vnd.oasis.opendocument.text-web application/vnd.oasis.opendocument.text-master application/vnd.oasis.opendocument.text-master-template application/vnd.openxmlformats-officedocument.wordprocessingml.document application/msword
+xdg-mime default libreoffice-calc.desktop application/vnd.oasis.opendocument.spreadsheet application/vnd.oasis.opendocument.spreadsheet-template application/vnd.openxmlformats-officedocument.spreadsheetml.sheet application/vnd.ms-excel
 xdg-mime default libreoffice-impress.desktop application/vnd.oasis.opendocument.presentation application/vnd.oasis.opendocument.presentation-template
 
 mkdir -p ~/.config/beets
